bd44326c03d819a53d5f294ed0cca7e240d4ecf0
omerpeled6 <opeled6@gmail.com>
2024-11-16 16:50:39 +0200

    update

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 to run the project
 
-update the env file in mongoUri to my password
-
 open 2 termminal
 1 -> backend -> npm run dev
 2 -> frontend -> npm start
--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 const userRoutes = require('./routes/user');
-const path = require('path');
 
 // express app
 const app = express();
-// const __dirname = path.resolve();
 
 // middleware
 app.use(express.json());
@@ -21,14 +19,6 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes);
 app.use('/api/user', userRoutes);
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-  });
-}
 
 // connect to db
 mongoose
