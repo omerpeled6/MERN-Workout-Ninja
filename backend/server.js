a8efe7f20672e1ad9c3421e5a6dc5a94890b8ffe
omerpeled6 <opeled6@gmail.com>
2024-11-19 16:36:39 +0200

    d

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,26 @@
 require('dotenv').config()
-
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
 
-// express app
 const app = express()
 const PORT = process.env.PORT || 3000
 
-// middleware
 app.use(express.json())
-
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
 
-// routes
 app.use('/api/workouts', workoutRoutes)
 app.use('/api/user', userRoutes)
 
-// connect to db
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    // listen for requests
-    app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
+    app.listen(PORT, () => {
+      console.log(`Connected to DB & listening on port ${PORT}`)
     })
   })
   .catch((error) => {
