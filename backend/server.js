b5b2476bd5df31aa7c6edc1fd40870150ed82d0c
omerpeled6 <opeled6@gmail.com>
2024-11-24 09:46:16 +0200

    d

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-const path = require('path')
-
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
 
@@ -21,27 +19,15 @@ app.use((req, res, next) => {
 app.use('/api/workouts', workoutRoutes)
 app.use('/api/user', userRoutes)
 
-// Serve React frontend in production
-if (process.env.NODE_ENV === 'production') {
-  const frontendPath = path.join(__dirname, './build')
-  app.use(express.static(frontendPath))
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(frontendPath, 'index.html'))
-  })
-}
-
 // connect to db
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
+    // listen for requests
     app.listen(process.env.PORT, () => {
-      console.log('Connected to DB & listening on port', process.env.PORT)
+      console.log('connected to db & listening on port', process.env.PORT)
     })
   })
   .catch((error) => {
-    console.error('Database connection error:', error)
+    console.log(error)
   })
