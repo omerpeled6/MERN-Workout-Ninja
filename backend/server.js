bf10b0bffe66b5660815c740ab4fadde30b9a941
omerpeled6 <opeled6@gmail.com>
2024-11-24 08:54:27 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -26,8 +26,8 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
-    app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
+    app.listen(PORT, () => {
+      console.log('connected to db & listening on port', PORT)
     })
   })
   .catch((error) => {
